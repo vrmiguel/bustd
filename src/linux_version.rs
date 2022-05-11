07f6060c928f6e4849379aab7cc6e8be9cd6d772
Vinícius Rodrigues Miguel <lemao.vrm07@hotmail.com>
2022-05-11 13:06:48 -0300

    impl Display for LinuxVersion

--- src/linux_version.rs
+++ src/linux_version.rs
@@ -1,5 +1,14 @@
+use std::fmt::Display;
+
 #[derive(Debug)]
 pub struct LinuxVersion {
     pub major: u8,
     pub minor: u8,
+}
+
+impl Display for LinuxVersion {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let (major, minor) = (self.major, self.minor);
+        write!(f, "Linux {major}.{minor}")
+    }
 }
